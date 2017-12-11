aa0de25bb395c731352b2060045a0347ae2d23a8
thanle95 <lengocthan95@gmail.com>
2017-12-11 22:16:41 +0700

    initialize UI

---
+++ PhanQuyen/PhanQuyen/LoginWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PhanQuyen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LogInWindow : Window
+    {
+        public LogInWindow()
+        {
+            InitializeComponent();
+            cbbYear.Items.Add("2017");
+            cbbMonth.Items.Add("10");
+            cbbMonth.Items.Add("11");
+            cbbMonth.Items.Add("12");
+            cbbDate.Items.Add("01");
+            cbbDate.Items.Add("02");
+            cbbDate.Items.Add("03");
+            cbbDate.Items.Add("04");
+            cbbDate.Items.Add("05");
+            cbbDate.Items.Add("06");
+            cbbDate.Items.Add("07");
+            cbbDate.Items.Add("08");
+            
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+
+        {
+
+            if (txtbUsername.Text.Equals("tanhoa") && txtbPassword.Password.Equals("123"))
+            {
+                //MessageBox.Show("Đăng nhập thành công");
+                this.Hide();
+                new MainWindow().Show();
+            }
+        }
+
+     
+    }
+}
---
+++ PhanQuyen/PhanQuyen/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PhanQuyen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void btnExpandRibbon_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.RibbonMain.IsMinimized)
+            {
+                this.btnExpandRibbon.SmallImageSource = new BitmapImage(new Uri("/PhanQuyen;component/Images/up.png", UriKind.Relative));
+                this.RibbonMain.IsMinimized = false;
+            }
+            else
+            {
+                this.btnExpandRibbon.SmallImageSource = new BitmapImage(new Uri("/PhanQuyen;component/Images/down.png", UriKind.Relative));
+                this.RibbonMain.IsMinimized = true;
+            }
+        }
+    }
+}
