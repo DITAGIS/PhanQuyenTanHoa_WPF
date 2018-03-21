9b044bf5b2441cd4b11c6099689a92bc15e210bc
thanle95 <lengocthan95@gmail.com>
2018-03-21 11:22:02 +0700

    cap nhat title

--- PhanQuyen/PhanQuyen/WindowView/MainWindow.xaml.cs
+++ PhanQuyen/PhanQuyen/WindowView/MainWindow.xaml.cs
@@ -41,7 +41,7 @@ namespace PhanQuyen
         UC_ThongKeDHNTheoDotSo uc_ThongKeDHNTheoDotSo;
         UC_XuatDuLieuRaSmartPhone uc_XuatDuLieuRaSmartPhone;
         private MyUser user;
-
+        private String title = "Phần mềm kết nối ứng dụng đọc số trên Smartphone            Nhân viên: ";
         public MainWindow()
         {
 
@@ -52,7 +52,8 @@ namespace PhanQuyen
         public MainWindow(MyUser user)
         {
             this.user = user; InitializeComponent();
-            this.Title += "             Nhân viên: " + user.UserName;
+            this.title += user.UserName;
+            this.Title = title;
         }
 
         private void Window_Closed(object sender, EventArgs e)
@@ -175,6 +176,7 @@ namespace PhanQuyen
             if (stkMain.Children.Count == 1)
                 stkMain.Children.RemoveAt(0);
             stkMain.Children.Add(uc_DieuChinhThonTinDocSo);
+            this.Title = this.title  + "            Điều chỉnh thông tin đọc số";
         }
 
         private void ribBtnInPhieuTieuThuKH_Click(object sender, RoutedEventArgs e)
@@ -194,6 +196,8 @@ namespace PhanQuyen
             if (stkMain.Children.Count == 1)
                 stkMain.Children.RemoveAt(0);
             stkMain.Children.Add(uc_InPhieuTieuThuKH);
+
+            this.Title = this.title + "             In phiếu tiêu thụ khách hàng";
             uc_InPhieuTieuThuKH.Print();
         }
 
@@ -205,6 +209,7 @@ namespace PhanQuyen
             resizeUC();
             if (stkMain.Children.Count == 1)
                 stkMain.Children.RemoveAt(0);
+            this.Title = this.title + "             Cập nhật hóa đơn";
             stkMain.Children.Add(uc_CapNhatHoaDon);
         }
 
@@ -217,6 +222,7 @@ namespace PhanQuyen
             if (stkMain.Children.Count == 1)
                 stkMain.Children.RemoveAt(0);
             stkMain.Children.Add(uc_NhanDuLieu);
+            this.Title = this.title + "             Nhận dữ liệu";
             uc_NhanDuLieu.ShowGetDataWindow();
         }
         private void ribBtnThongKeSauDocSo_Click(object sender, RoutedEventArgs e)
@@ -227,6 +233,7 @@ namespace PhanQuyen
             if (stkMain.Children.Count == 1)
                 stkMain.Children.RemoveAt(0);
             stkMain.Children.Add(uc_ThongKeDHNSauDocSo);
+            this.Title = this.title + "             Thống kê ĐHN sau đọc số";
         }
         private void ribBtnBaoCaoTongHop_Click(object sender, RoutedEventArgs e)
         {
@@ -236,6 +243,7 @@ namespace PhanQuyen
             if (stkMain.Children.Count == 1)
                 stkMain.Children.RemoveAt(0);
             stkMain.Children.Add(uc_BaoCaoTongHop);
+            this.Title = this.title + "             Báo cáo tổng hợp";
         }
         private void ExitCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
@@ -263,6 +271,7 @@ namespace PhanQuyen
             if (stkMain.Children.Count == 1)
                 stkMain.Children.RemoveAt(0);
             stkMain.Children.Add(uc_ChuyenBilling);
+            this.Title = this.title + "             Chuyển billing";
         }
 
         private void ribBtnInDanhSachDongCua_Click(object sender, RoutedEventArgs e)
@@ -273,6 +282,7 @@ namespace PhanQuyen
             if (stkMain.Children.Count == 1)
                 stkMain.Children.RemoveAt(0);
             stkMain.Children.Add(uc_InDanhSachDongCua);
+            this.Title = this.title + "             In danh sách đóng cửa";
         }
         private void ribBtnXuatDuLieu_Click(object sender, RoutedEventArgs e)
         {
@@ -282,6 +292,7 @@ namespace PhanQuyen
             if (stkMain.Children.Count == 1)
                 stkMain.Children.RemoveAt(0);
             stkMain.Children.Add(uc_XuatDuLieuRaSmartPhone);
+            this.Title = this.title + "             Tạo dữ liệu đọc số, xuất dữ liệu ra Smartphone";
         }
         private void ribBtnKiemTraDuLieu_Click(object sender, RoutedEventArgs e)
         {
@@ -291,6 +302,7 @@ namespace PhanQuyen
             if (stkMain.Children.Count == 1)
                 stkMain.Children.RemoveAt(0);
             stkMain.Children.Add(uc_KiemTraDuLieu);
+            this.Title = this.title + "             Kiểm tra dữ liệu";
         }
         private void ribBtnThongKeDHNTheoDotSo_Click(object sender, RoutedEventArgs e)
         {
@@ -300,6 +312,7 @@ namespace PhanQuyen
             if (stkMain.Children.Count == 1)
                 stkMain.Children.RemoveAt(0);
             stkMain.Children.Add(uc_ThongKeDHNTheoDotSo);
+            this.Title = this.title + "             Thống kê ĐHN theo đợt sổ";
         }
         private void ribBtnInTieuThuBatThuong_Click(object sender, RoutedEventArgs e)
         {
@@ -309,6 +322,7 @@ namespace PhanQuyen
             if (stkMain.Children.Count == 1)
                 stkMain.Children.RemoveAt(0);
             stkMain.Children.Add(uc_InTieuThuBatThuong);
+            this.Title = this.title + "             In phiếu tiêu thụ bất thường";
         }
         private void ribBtnThongKeDHNTrenMang_Click(object sender, RoutedEventArgs e)
         {
@@ -318,6 +332,7 @@ namespace PhanQuyen
             if (stkMain.Children.Count == 1)
                 stkMain.Children.RemoveAt(0);
             stkMain.Children.Add(uc_ThongKeDHNTrenMang);
+            this.Title = this.title + "             Thống kê ĐHN trên mạng";
         }
         private void ribBtnHoanTatDocSo_Click(object sender, RoutedEventArgs e)
         {
@@ -353,9 +368,9 @@ namespace PhanQuyen
             }
         }
 
-     
 
-     
+
+
     }
 
     public static class CustomCommands
